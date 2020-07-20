466e61d08811f6280efd11fd042c12c68eaf5f08
sergiusglad <sergius.glad@yandex.ru>
2020-07-20 13:59:20 +0300

    Vue + Изменения названия файлов

---
+++ js/about.f10f5152.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"10c2":function(e,t,n){"use strict";n.r(t);var l=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("h1",[e._v("Вопрос ответ")])},u=[],c=n("2877"),r={},a=Object(c["a"])(r,l,u,!1,null,null,null);t["default"]=a.exports},c3ef:function(e,t,n){"use strict";n.r(t);var l=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("h1",[e._v("Помощь")])},u=[],c=n("2877"),r={},a=Object(c["a"])(r,l,u,!1,null,null,null);t["default"]=a.exports}}]);
+//# sourceMappingURL=about.f10f5152.js.map
